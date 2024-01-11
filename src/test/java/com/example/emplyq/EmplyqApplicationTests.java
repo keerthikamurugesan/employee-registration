9b6d10c96cb76c39e302a4c6b8f64f5b5abf5da4
keerthikamurugesan <114380482+keerthikamurugesan@users.noreply.github.com>
2024-01-11 10:17:24 +0530

    Add files via upload

---
+++ src/test/java/com/example/emplyq/EmplyqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emplyq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmplyqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
